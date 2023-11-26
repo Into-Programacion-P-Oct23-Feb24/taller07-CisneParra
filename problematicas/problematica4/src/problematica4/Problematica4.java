728b913073425e1e4455a17c069e6bf96ee5faeb
Cisne Parra <adparra2@utpl.edu.ec>
2023-11-26 16:29:53 -0500

    cambios de la tarea

---
+++ problematicas/problematica4/src/problematica4/Problematica4.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problematica4;
+
+/**
+ *
+ * @author natsu
+ */
+public class Problematica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int limite = 15;
+        int numerador = 1;
+        int denominador = 3;
+        int contador = 0;
+        double fraccion;
+        double total = 1;
+        String signo;
+        String cadena = "";
+        while (denominador <= limite) {
+            if (contador == 0) {
+                signo = "-";
+                fraccion = (-((double) numerador / denominador));
+                contador = contador + 1;
+            } else {
+                signo = "+";
+                contador = contador - 1;
+                fraccion = (-((double) numerador / denominador));
+            }
+            cadena = String.format("%s%s %d/%d", cadena,
+                    signo, numerador, denominador);
+            total = total + fraccion;
+            denominador = denominador + 2;
+        }
+        System.out.printf("%d %s\nCon un resultado"
+                + " de: %.2f\n", contador, cadena, total);
+    }
+
+}
